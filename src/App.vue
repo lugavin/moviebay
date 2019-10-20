0a62c6af0c8681c5c35642c1037eb3fe71b85fc1
Gavin <lugavin@outlook.com>
2019-10-21 07:27:14 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span class="font-weight-light">影视湾</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                <span class="mr-2">Latest Release</span>
+            </v-btn>
+        </v-app-bar>
+        <v-content>
+            <HelloWorld/>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+    data: () => ({
+        //
+    }),
+};
+</script>
