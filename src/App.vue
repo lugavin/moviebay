b073d4192275f68dbeb12fff9d33c242d8b48944
Gavin <lugavin@outlook.com>
2019-11-03 22:55:52 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
     <v-app>
-        <v-app-bar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span class="font-weight-light">影视湾</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-                <span class="mr-2">Latest Release</span>
-            </v-btn>
+        <!-- app属性: 设置向下滚动时保持在顶部 -->
+        <v-app-bar app flat dense>
+            <v-container>
+                <v-layout>
+                    <v-toolbar-items>
+                        <v-img :src="require('@/assets/logo.png')"
+                               contain class="mr-5" height="48" width="180" max-width="180"/>
+                        <v-btn text to="/home">首页</v-btn>
+                        <v-btn text to="/movie">电影</v-btn>
+                        <v-btn text to="/tv">连续剧</v-btn>
+                        <v-btn text to="/variety">综艺</v-btn>
+                        <v-btn text to="/anime">动漫</v-btn>
+                        <v-btn text to="/news">资讯</v-btn>
+                        <v-btn text to="/comments">留言</v-btn>
+                        <v-text-field placeholder="请输入关键字" append-icon="mdi-magnify"
+                                      flat hide-details solo-inverted dense
+                                      style="max-width: 240px; padding: 5px 15px;"/>
+                        <v-spacer/>
+                        <v-btn text to="/login">
+                            <span class="mr-2">登录</span>
+                        </v-btn>
+                        <v-btn text to="/register">
+                            <span class="mr-2">注册</span>
+                        </v-btn>
+                    </v-toolbar-items>
+                </v-layout>
+            </v-container>
         </v-app-bar>
         <v-content>
-            <HelloWorld/>
+            <v-container>
+                <router-view/>
+            </v-container>
         </v-content>
     </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+/**
+ * @see https://cn.vuejs.org/v2/api/#name
+ */
 export default {
     name: 'App',
-    components: {
-        HelloWorld,
-    },
     data: () => ({
-        //
-    }),
+    })
 };
 </script>
