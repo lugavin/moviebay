38156919195e2cdc51b943a88d598a9dbd1c7299
Gavin <lugavin@outlook.com>
2019-12-09 23:55:47 +0800

    Update

--- front-end/src/components/video/VideoPlay.vue
+++ front-end/src/components/video/VideoPlay.vue
@@ -2,13 +2,13 @@
     <v-container>
         <v-row dense no-gutters>
             <v-col cols="12" md="9">
-                <video id="vjs-player" class="video-js vjs-fluid vjs-big-play-left-bottom" controls preload
+                <video id="vjs-player" class="video-js vjs-fluid vjs-styles-custom" controls preload
                        :poster="video.poster">
                     <source :src="video.url" type="video/mp4"/>
                     <source :src="video.url" type="video/webm"/>
                     <p class="vjs-no-js">
-                        To view this video please enable JavaScript, and consider upgrading to a web browser that
-                        <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                        To view this video please enable JavaScript, and consider upgrading to a web browser
+                        that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
                     </p>
                 </video>
                 <v-card tile flat dark>
@@ -16,7 +16,7 @@
                     <v-card-subtitle class="py-2">
                         8.4 / 2019 / 大陆 / 剧情 / 2048次播放
                         <a class="white--text" @click="expand=!expand">
-                            详情 <v-icon>{{expand?'keyboard_arrow_up':'keyboard_arrow_down'}}</v-icon>
+                            详情<v-icon>{{expand?'keyboard_arrow_up':'keyboard_arrow_down'}}</v-icon>
                         </a>
                     </v-card-subtitle>
                     <v-card-text :class="[{'d-none': !expand}]">
@@ -32,20 +32,21 @@
                     <v-tab>播放列表</v-tab>
                     <v-tab>相关推荐</v-tab>
                     <v-tab-item>
-                        <v-card tile flat dark class="v-card-fixed-height">
+                        <v-card tile flat dark class="v-card-content hide-scrollbar">
                             <v-card-text>
                                 <v-btn color="primary">1080P</v-btn>
                             </v-card-text>
                         </v-card>
                     </v-tab-item>
                     <v-tab-item>
-                        <v-card tile flat dark class="v-card-fixed-height">
+                        <v-card tile flat dark class="v-card-content hide-scrollbar">
                             <v-container>
                                 <v-row dense>
                                     <v-col v-for="(video, i) in videos" :key="i" cols="3" md="6">
-                                        <v-card outlined>
+                                        <v-card flat outlined>
                                             <v-hover v-slot:default="{ hover }">
-                                                <v-img :src="require(`@/assets/img/${video.poster}`)" class="align-end text-center">
+                                                <v-img :src="require(`@/assets/img/${video.poster}`)"
+                                                       class="align-end text-center">
                                                     <v-expand-transition>
                                                         <div class="d-flex transition-fast-in-fast-out grey darken-3 v-card--reveal"
                                                              v-if="hover">
@@ -96,41 +97,71 @@ export default {
 <style>
 @import "~video.js/dist/video-js.min.css";
 
-.vjs-big-play-left-bottom .vjs-big-play-button {
+.vjs-styles-custom .vjs-big-play-button {
     top: auto;
-    left: 1vw;
-    bottom: 1vw;
-    font-size: 2vw;
+}
+
+/*
+.scrollbar-outer-container {
+    overflow: hidden;
+}
+
+.scrollbar-inner-container {
+    margin-right: -18px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+}
+*/
+
+.hide-scrollbar {
+    overflow: auto;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+}
+
+.hide-scrollbar::-webkit-scrollbar {
+    display: none;
 }
 
 @media only screen and (max-width: 959px) {
-    .vjs-big-play-left-bottom .vjs-big-play-button {
-        top: auto;
-        left: 2vw !important;
+    .vjs-styles-custom .vjs-big-play-button {
+        left: 1vw !important;
         bottom: 2vw !important;
         font-size: 3vw !important;
     }
 }
 
-@media only screen and (min-width: 960px) {
-    .v-card-fixed-height {
-        height: 410px;
-        overflow: auto;
+@media only screen and (min-width: 960px) and (max-width: 1263px) {
+    .vjs-styles-custom .vjs-big-play-button {
+        left: 0.5vw !important;
+        bottom: 0.8vw !important;
+        font-size: 2vw !important;
     }
 
-    .v-card-fixed-height::-webkit-scrollbar {
-        display: none;
+    .v-card-content {
+        height: 410px !important;
     }
 }
 
-@media only screen and (min-width: 1264px) {
-    .v-card-fixed-height {
-        height: 530px;
-        overflow: auto;
+@media only screen and (min-width: 1264px) and (max-width: 1903px) {
+    .vjs-styles-custom .vjs-big-play-button {
+        left: 10px;
+        bottom: 10px;
     }
 
-    .v-card-fixed-height::-webkit-scrollbar {
-        display: none;
+    .v-card-content {
+        height: 530px !important;
+    }
+}
+
+@media only screen and (min-width: 1904px) {
+    .vjs-styles-custom .vjs-big-play-button {
+        left: 20px !important;
+        bottom: 20px !important;
+    }
+
+    .v-card-content {
+        height: 783px !important;
     }
 }
 </style>
