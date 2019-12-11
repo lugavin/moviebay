06525985c72b01649af832c2004729d75d790d52
Gavin Lu <lugavin@users.noreply.github.com>
2019-12-11 23:31:32 +0800

    GitHub Pages Deploy Action

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
     configureWebpack: {},
     transpileDependencies: [
         "vuetify"
-    ]
+    ],
+    publicPath: process.env.NODE_ENV === 'production' ? '/repository-name/' : '/'
 };
