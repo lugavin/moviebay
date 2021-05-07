a650abb50916641dd5476230067265ad0651f7ed
Gavin <lugavin@outlook.com>
2021-05-07 14:38:39 +0800

    Update

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,14 +1,20 @@
 import {NestFactory} from '@nestjs/core';
 import {Logger} from '@nestjs/common';
+import {SwaggerModule} from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 import {AppModule} from './app.module';
 import {ConfigFactory} from './config';
-import {SwaggerModule} from '@nestjs/swagger';
 
 /**
+ * Helmet can help protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately.
+ * RateLimit can help protect your applications from brute-force attacks.
  * @see https://docs.nestjs.com/faq/request-lifecycle
  */
 (async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.use(rateLimit());
     SwaggerModule.setup(process.env.SWAGGER_API_PATH, app, SwaggerModule.createDocument(app, {
         openapi: process.env.SWAGGER_API_SPEC,
         info: {
